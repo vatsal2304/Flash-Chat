7833f520a2b58dc7ae6f019cdb6176d721491636
vatsal <116101020+vatsal2304@users.noreply.github.com>
2023-08-09 12:28:20 +0530

    Add files via upload

---
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/message.swift
@@ -0,0 +1,14 @@
+//
+//  message.swift
+//  Flash Chat iOS13
+//
+//  Created by Funnmedia 2 on 18/07/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct message {
+    let sender: String
+    let body: String
+}
